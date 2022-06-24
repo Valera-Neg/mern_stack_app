0fb005786ed386e15c0905a11491e8d15d4055a4
Valeriy Nehovora <nehovora.val@gnail.com>
2022-06-24 13:58:36 -0400

    Creating and commecting mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routs/workouts.js");
 
 //express app
@@ -15,10 +16,17 @@ app.use((req, res, next) => {
 });
 
 //rourtes
-
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listeneing on port", process.env.PORT);
-});
+//connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listeneing on port", process.env.PORT);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
