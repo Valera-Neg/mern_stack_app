7df512b76b15e3aa96ec82ab6216a79b1449ee09
Valeriy Nehovora <nehovora.val@gnail.com>
2022-06-25 19:08:00 -0400

    adding delete & update routes

--- backend/routs/workouts.js
+++ backend/routs/workouts.js
@@ -3,6 +3,8 @@ const {
   createWorkout,
   getAllWorkouts,
   getSingleWorkout,
+  deleteWorkout,
+  updateWorkout,
 } = require("../controllers/workoutConrtroller");
 
 const router = express.Router();
@@ -17,13 +19,9 @@ router.get("/:id", getSingleWorkout);
 router.post("/", createWorkout);
 
 //DELETE a workout
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE a workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 //UPDATE a workout
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE a workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
